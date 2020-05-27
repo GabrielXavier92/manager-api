578a031b3cf3226858095b675030fa0351a89800
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-05-27 18:09:29 -0300

    add accountId to select result on addSelect

--- src/utils/addSelect/index.ts
+++ src/utils/addSelect/index.ts
@@ -4,6 +4,8 @@ const addSelect = (next: any) => (root: any, args: any, context: any, info: any)
   const result = new PrismaSelect(info).value;
   let newArgs;
   if (Object.keys(result.select).length > 0) {
+    // Adiciona accountId true para ser usado nas verificacoes dos resolvers
+    result.select.accountId = true;
     newArgs = {
       ...args,
       select: result,
