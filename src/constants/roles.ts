5ca0e35d238f51f330c0d976e0611b53fd5a829f
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-06-26 19:24:28 -0300

    create default roles

---
+++ src/constants/roles.ts
@@ -0,0 +1,16 @@
+const roles = [
+  { role: 'GET_DOCTOR' },
+  { role: 'GET_DOCTORS' },
+  { role: 'CREATE_DOCTOR' },
+  { role: 'UPDATE_DOCTOR' },
+  { role: 'GET_GUIDE' },
+  { role: 'GET_GUIDES' },
+  { role: 'CREATE_GUIDE' },
+  { role: 'UPDATE_GUIDE' },
+  { role: 'GET_PATIENT' },
+  { role: 'GET_PATIENTS' },
+  { role: 'CREATE_PATIENT' },
+  { role: 'UPDATE_PATIENT' },
+];
+
+export default roles;
