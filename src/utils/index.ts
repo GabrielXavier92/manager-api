30df14cdb3f17db937ad28732895583d7a40bb5a
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-06-26 19:42:36 -0300

    create default exports for utils

---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+export { default as addSelect } from './addSelect';
+export { default as authenticated } from './authenticated';
+export { default as authorizated } from './authorizated';
+export { default as createContext } from './createContext';
+export { default as decodeJWT } from './decodeJWT';
