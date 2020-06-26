bd77395df883a311fd18c8467775e2feed83a6c5
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-06-26 19:42:25 -0300

    create logic of authorizated

---
+++ src/utils/authorizated/index.ts
@@ -0,0 +1,16 @@
+import { ForbiddenError } from 'apollo-server';
+
+export const authorizated = (role: string) => (next: any) => (
+  root: any,
+  args: any,
+  context: any,
+  info: any,
+) => {
+  if (!context.currentUser.roles.includes(role)) {
+    throw new ForbiddenError('Unauthorized!');
+  }
+
+  return next(root, args, context, info);
+};
+
+export default authorizated;
