65a39fbad31ae8a5a691058e620d2e8227d88f55
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-06-26 19:24:17 -0300

    create scrip tp seed dn

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,15 +1,21 @@
+import 'dotenv/config';
+
 import { PrismaClient } from "@prisma/client";
+import roles from '../src/constants/roles'
 
 const prisma = new PrismaClient();
 
+
 async function seedData() {
   await prisma.connect();
-  await prisma.roles.create({
-    data: {
-      role: "ADMIN"
-    }
-  });
-  await prisma.disconnect();
+
+    roles.forEach(async role => {
+      await prisma.roles.create({
+        data: role
+      })
+    })
+  
+    await prisma.disconnect();
 }
 
 seedData();
