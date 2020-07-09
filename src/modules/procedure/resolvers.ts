43e49df571088d6f1dd0265a6ef3a53ac0273dc6
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-07-08 21:38:17 -0300

    use created methods

--- src/modules/procedure/resolvers.ts
+++ src/modules/procedure/resolvers.ts
@@ -1,18 +1,42 @@
 import { ResolverMap } from '../../types/graphql-utils';
 
+import getProcedureTable from './resolver/getProcedureTable';
+import getProcedureTables from './resolver/getProcedureTables';
+import createProcedureTable from './resolver/createProcedureTable';
+import updateProcedureTable from './resolver/updateProcedureTable';
+
+import getProcedure from './resolver/getProcedure';
+import getProcedures from './resolver/getProcedures';
+import createProcedure from './resolver/createProcedure';
+import updateProcedure from './resolver/updateprocedure';
+
+import getSpecialty from './resolver/getSpecialty';
+import getSpecialties from './resolver/getSpecialties';
+import createSpecialty from './resolver/createSpecialty';
+import updateSpecialty from './resolver/updateSpecialty';
 
 const Resolver: ResolverMap = {
   Query: {
-    getProcedureTable: (id: string) => { console.log(id); },
-    getProcedureTables: () => { console.log('getproceduretables'); },
-    getProcedures: (tableId?: string) => { console.log(tableId); },
-    getProcedure: (id: string) => { console.log(id); },
+    // Tabela
+    getProcedureTable,
+    getProcedureTables,
+    // Procedimentos
+    getProcedures,
+    getProcedure,
+    // Especialidades
+    getSpecialty,
+    getSpecialties,
   },
   Mutation: {
-    createProcedureTable: () => {},
-    updateProcedureTable: () => {},
-    createProcedure: () => {},
-    updateProcedure: () => {},
+    // Tabela
+    createProcedureTable,
+    updateProcedureTable,
+    // Procedimento
+    createProcedure,
+    updateProcedure,
+    // Especialidades
+    createSpecialty,
+    updateSpecialty,
   },
 };
 
