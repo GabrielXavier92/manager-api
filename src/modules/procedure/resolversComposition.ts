e1b7f9904b53fe86e61d54a2853c877c7b02bf43
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-07-08 21:42:39 -0300

    add roles to procedure module

--- src/modules/procedure/resolversComposition.ts
+++ src/modules/procedure/resolversComposition.ts
@@ -2,9 +2,20 @@ import { authenticated, authorizated } from '../../utils';
 
 const resolversComposition = {
   'Query.getProcedureTable': [authenticated, authorizated('GET_PROCEDURE_TABLE')],
-  // 'Query.getSpecialties': [authenticated, authorizated('GET_SPECIALTIES')],
-  // 'Mutation.createSpecialty': [authenticated, authorizated('CREATE_SPECIALTY')],
-  // 'Mutation.updateSpecialty': [authenticated, authorizated('UPDATE_SPECIALTY')],
+  'Query.getProcedureTables': [authenticated, authorizated('GET_PROCEDURE_TABLES')],
+  'Mutation.createProcedureTable': [authenticated, authorizated('CREATE_PROCEDURE_TABLE')],
+  'Mutation.updateProcedureTable': [authenticated, authorizated('UPDATE_PROCEDURE_TABLE')],
+
+  'Query.getProcedure': [authenticated, authorizated('GET_PROCEDURE')],
+  'Query.getProcedures': [authenticated, authorizated('GET_PROCEDURES')],
+  'Mutation.createProcedure': [authenticated, authorizated('CREATE_PROCEDURE')],
+  'Mutation.updateProcedure': [authenticated, authorizated('UPDATE_PROCEDURE')],
+
+  'Query.getSpecialty': [authenticated, authorizated('GET_SPECIALTY')],
+  'Query.getSpecialties': [authenticated, authorizated('GET_SPECIALTIES')],
+  'Mutation.createSpecialty': [authenticated, authorizated('CREATE_SPECIALTY')],
+  'Mutation.updateSpecialty': [authenticated, authorizated('UPDATE_SPECIALTY')],
+
 };
 
 export default resolversComposition;
