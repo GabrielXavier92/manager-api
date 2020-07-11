e4be3c8eeca799f8cff1dfd516ce2fd3af4870bd
Gabriel Xavier <gabriel.xavier@hotmart.com>
2020-07-11 17:24:51 -0300

    add query to patients resolver

--- src/modules/patient/resolvers/patients.ts
+++ src/modules/patient/resolvers/patients.ts
@@ -1,18 +1,41 @@
 import { ForbiddenError } from 'apollo-server';
-import { Patient } from '../../../types/types';
+import { GetPatients } from '../../../types/types.d';
 import { Resolver } from '../../../types/graphql-utils';
 
-const getPatients: Resolver = async (_, { fields }, { prisma, user }): Promise<Array<Patient>> => {
+const getPatients: Resolver = async (_, {
+  take = 10,
+  cursor,
+  filter, fields,
+}, { prisma, user }): Promise<GetPatients> => {
   try {
-    const patients = await prisma.patient.findMany({
-      where: {
-        accountId: user?.accountId,
-      },
-      ...fields,
-    });
+    let patients;
+    if (fields.select.patients) {
+      patients = await prisma.patient.findMany({
+        take,
+        skip: cursor ? 1 : 0,
+        cursor: cursor ? { id: cursor } : undefined,
+        where: {
+          accountId: user?.accountId,
+          name: { contains: filter },
+        },
+        orderBy: { name: 'asc' },
+        ...fields.select.patients,
+      });
+    }
 
-    return patients;
+    let ammount = 0;
+    if (fields.select.queryInfo) {
+      ammount = await prisma.patient.count({
+        where: {
+          accountId: user?.accountId,
+          name: { contains: filter },
+        },
+      });
+    }
+
+    return { patients, queryInfo: { ammount } };
   } catch (e) {
+    console.log(e);
     throw new ForbiddenError('Falha ao buscar paciente');
   }
 };
